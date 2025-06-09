384bcfde500468b5705a5589910f0ca0788c933f
Darkcraft8 <phoenixdarkcraft8spqr@gmail.com>
2025-06-09 14:04:41 +0200

    Fixed Incompatibility with a few vanilla script maching trick for the scriptLoader

--- items/active/stationtransponder/stationtransponder.lua
+++ items/active/stationtransponder/stationtransponder.lua
@@ -1,14 +1,26 @@
+require ("/items/active/stationtransponder/jpbai_scriptLoader.lua")
+local jpbai_scriptLoaderActif = false
 function init()
-  if config.getParameter("replacementScripts") then
-	if type(config.getParameter("replacementScripts")) == "table" then
-		for _, scriptPath in ipairs(config.getParameter("replacementScripts") or {}) do 
-			require (scriptPath)
-		end
-	else
-		require (config.getParameter("replacementScripts"))
-	end
-  else
-	require ("/items/active/stationtransponder/vanilla_stationtransponder.lua")
+  if config.getParameter("replacementScripts") then loadScript() jpbai_scriptLoaderActif = true return end
+  if storage.consumed then
+    item.consume(1)
+    return
   end
-  init()
+  storage.consumed = false
+
+  message.setHandler("holdingTransponder", function() return true end)
+  message.setHandler("setTransponderConsumed", function() storage.consumed = true end)
+  message.setHandler("consumeTransponder", function() item.consume(1) end)
+end
+
+function activate(fireMode, shiftHeld)
+  if jpbai_scriptLoaderActif then return end
+  activeItem.interact(config.getParameter("interactAction"), config.getParameter("interactData"));
+end
+
+function uninit()
+  if jpbai_scriptLoaderActif then return end
+  if storage.consumed and item.count() > 0 then
+    item.consume(1)
+  end
 end
